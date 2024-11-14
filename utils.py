ceddf1015a113474c7397730d308e84ebc60c630
unknown <pkomals94@gmail.com>
2024-11-14 13:52:58 +0530

    merging master to main

--- utils.py
+++ utils.py
@@ -12,6 +12,6 @@ def get_str_from_food_dict(food_dict: dict):
     result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
     return result
 
-if __name__=="__main__":
-    print(get_str_from_food_dict({"samosa":2,"lassi":3}))
-#     print (extract_session_id("projects/foodfinder-edah/agent/sessions/247a440e-96d5-c77e-c8f2-8231a0ce0f69/contexts/ongoingorder"))
+# if __name__=="__main__":
+#     print(get_str_from_food_dict({"samosa":2,"lassi":3}))
+# #     print (extract_session_id("projects/foodfinder-edah/agent/sessions/247a440e-96d5-c77e-c8f2-8231a0ce0f69/contexts/ongoingorder"))
